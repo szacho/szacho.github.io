bc2b782c01294f6952be48bfbfba4e8885c33c30
Michał Szachniewicz <mszachniewicz@outlook.com>
2022-08-28 19:43:09 +0200

    refactor: migrate from Gatsby to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://szacho.github.io',
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+    },
+  },
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }), sitemap(), react(), image()],
+});
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faKaggle, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
+const SocialMedia = () => (
+<ul className="sidebar--social-media">
+    <li>
+        <a href='https://github.com/szacho/'><FontAwesomeIcon icon={faGithub} className="sidebar--social-media-icon"/></a>    
+    </li>
+    <li>
+        <a href='https://www.kaggle.com/szacho/'><FontAwesomeIcon icon={faKaggle} className="sidebar--social-media-icon"/></a>    
+    </li>
+    <li>
+        <a href='https://www.linkedin.com/in/mszachniewicz/'><FontAwesomeIcon icon={faLinkedinIn} className="sidebar--social-media-icon"/></a>    
+    </li>
+</ul>
+)
+
+export default SocialMedia
