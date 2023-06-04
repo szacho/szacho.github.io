ef51a1dfb71397cbc9dd081b2f62a478ed6fa102
Michał Szachniewicz <mszachniewicz@outlook.com>
2023-06-04 13:44:05 +0200

    chore: change site title

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'think before you die';
+export const SITE_TITLE = 'szacho\'s space';
 export const SITE_DESCRIPTION = 'artificial intelligence, internet of things, backpacking, and more';
 export const SITE_LOGO = '/logo.jpeg';
