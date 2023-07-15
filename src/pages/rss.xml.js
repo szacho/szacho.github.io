53de05d8b098b65cf772375239f622338b81c48f
Michał Szachniewicz <mszachniewicz@outlook.com>
2023-07-15 20:16:38 +0200

    fix: path to posts in RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,21 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-const postImportResult = import.meta.glob('./blog/*.{md,mdx}', { eager: true });
+const postImportResult = import.meta.glob('./posts/*.{md,mdx}', { eager: true });
 const posts = Object.values(postImportResult).sort(
-	(a, b) => new Date(b.frontmatter.date).valueOf() - new Date(a.frontmatter.date).valueOf()
+    (a, b) => new Date(b.frontmatter.date).valueOf() - new Date(a.frontmatter.date).valueOf()
 );
 
-export const get = () => rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: posts.map((post) => ({
-        link: post.url,
-        title: post.frontmatter.title,
-        pubDate: post.frontmatter.date,
-    })),
-    stylesheet: '/rss-styles.xsl'
-  });
+export async function get(context) {
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: import.meta.env.SITE,
+        items: posts.map((post) => ({
+            link: post.url,
+            title: post.frontmatter.title,
+            pubDate: post.frontmatter.date,
+        })),
+        stylesheet: '/rss-styles.xsl'
+    })
+};
